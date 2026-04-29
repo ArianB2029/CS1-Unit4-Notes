81b3a66024dd3f1380994a770a9cdf3843794543
ArianB2029 <arian.berisha2029@gbwl.org>
2026-04-29 15:42:27 +0000

    conditionals and boolean expressions

--- script.js
+++ script.js
@@ -90,3 +90,45 @@ console.log(mySando);
 
 // shortcut to see return values
 console.log( makeSando("pb", "j", "wonderbread") );
+
+/* Selection/branching (CONDITIONALS) A BOOLEAN EXPRESSION is like a QUESTION that has a boolean ANSWER (true/false)
+*/
+
+console.log(1 > 2); // false is the ANSWER
+console.log("Is 10 less than 5?");
+console.log(10 < 5);
+
+// Check for EQUALITY using the == operator (or ===)
+console.log(1 == 1); // true!
+console.log(1 != 1); // false -> asking if NOT equal
+console.log("yes" == "yes"); // works for Strings!
+console.log("nick" != "duncan");
+
+// Strings can be compared too
+console.log("Arian" > "Carolyn");
+console.log("Zebra" > "Dog"); // true Z comes later than D
+console.log("Apple" < "Banana"); // true, A comes before B
+console.log("pelican" > "PELICAN"); // case matters!
+
+// Can use comparison operators btwn VARIABLES & values too
+let password = "something";
+console.log("ifhdhwio" == password);
+
+// IF STATEMENTS use conditionals/boolean expressions
+// to make DECISIONS about which code to execute (run)
+let userAge = 16;
+
+if (userAge >= 17) {
+    // code in the curly brackets only runs if TRUE!
+    console.log("you are old enough for a NY license");
+}
+
+// ELSE IF BLOCKS let you test multiple options
+else if (userAge == 16) {
+    console.log("you can get a learners permit!");
+}
+
+// ELSE BLOCK accounts for the "otherwise..." case
+else {
+    console.log("too young to drive");
+}
